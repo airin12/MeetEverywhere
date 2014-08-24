c920bea7ec359715453aa36149bbb42611e056e5
marcin <marbogster@gmail.com>
2014-08-24 23:07:13 +0200

    Dodano usuwanie znajomych z listy znajomych

--- Meet Everywhere/src/com/meetEverywhere/FriendsListAdapter.java
+++ Meet Everywhere/src/com/meetEverywhere/FriendsListAdapter.java
@@ -1,5 +1,6 @@
 package com.meetEverywhere;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import android.content.Context;
@@ -8,20 +9,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.meetEverywhere.common.User;
 
 public class FriendsListAdapter extends ArrayAdapter<User> {
 
 	private List<User> users;
+	private List<User> toDeledeFriends;
+	private List<CheckBox> checkBoxes;
 	private Context context;
+	private int actualMode;
 
 	public FriendsListAdapter(Context context, int resource, List<User> users) {
 		super(context, resource, users);
 		this.users = users;
-		this.context = context;
+		this.context = context;	
+		toDeledeFriends = new LinkedList<User>();
+		checkBoxes = new LinkedList<CheckBox>();
+		actualMode=FriendsListFragment.NORMAL_MODE;
 	}
 
 	@Override
@@ -37,12 +45,17 @@ public class FriendsListAdapter extends ArrayAdapter<User> {
 
 		LayoutInflater inflater = (LayoutInflater) context
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View row = inflater.inflate(R.layout.found_tags_content_info, null);
-
-		ImageView photo = (ImageView) row.findViewById(R.id.photo);
-		TextView nick = (TextView) row.findViewById(R.id.user_name);
-
-		photo.setImageBitmap(user.getPicture());
+		View row = inflater.inflate(R.layout.friends_list_content_info, null);
+		
+		TextView nick = (TextView) row.findViewById(R.id.friends_list_username);
+		CheckBox checkBox = (CheckBox) row.findViewById(R.id.friends_list_checkbox);
+		
+		if(actualMode==FriendsListFragment.DELETE_MODE)
+			checkBox.setVisibility(CheckBox.VISIBLE);
+		else if(actualMode==FriendsListFragment.NORMAL_MODE)
+			checkBox.setVisibility(CheckBox.GONE);
+		
+		checkBoxes.add(checkBox);
 		nick.setText(user.getNickname());
 
 		return row;
@@ -51,6 +64,29 @@ public class FriendsListAdapter extends ArrayAdapter<User> {
 
 	public List<User> getUsers() {
 		return users;
+	}
+	
+	public void toggle(int position) {
+		
+		User user = users.get(position);
+		if(toDeledeFriends.contains(user)){
+			toDeledeFriends.remove(user);
+		}else{
+			toDeledeFriends.add(user);
+		}
+	}
+	
+	public void removeFromFriends(){
+		for(User u: toDeledeFriends){
+			u.setAcquaintance(false);
+			Toast.makeText(context, "Usuniêto z listy znajomych", Toast.LENGTH_SHORT).show();
+		}
+		
+		notifyDataSetChanged();
+	}
+	
+	public void changeMode(int mode){
+		actualMode=mode;
 	}
 
 }
--- Meet Everywhere/src/com/meetEverywhere/FriendsListFragment.java
+++ Meet Everywhere/src/com/meetEverywhere/FriendsListFragment.java
@@ -9,6 +9,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import com.meetEverywhere.bluetooth.BluetoothDispatcher;
@@ -18,6 +20,10 @@ import com.meetEverywhere.common.User;
 public class FriendsListFragment extends Activity {
 
 	private ListView listView;
+	private LinearLayout confirmButton;
+	public static final int NORMAL_MODE=1;
+	public static final int DELETE_MODE=2;
+	private int actualMode;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -25,7 +31,11 @@ public class FriendsListFragment extends Activity {
 		setContentView(R.layout.friends_list_fragment_layout);
 
 		listView = (ListView) findViewById(R.id.friends_list);
-
+		confirmButton= (LinearLayout) findViewById(R.id.confirm_button_row);
+		confirmButton.setVisibility(LinearLayout.GONE);
+		
+		actualMode=NORMAL_MODE;
+		
 		List<User> friends = Configuration.getInstance()
 				.getAcquaintancesUsers();
 
@@ -33,11 +43,15 @@ public class FriendsListFragment extends Activity {
 		Log.d("STARTING", "FRIENDS");
 
 		FriendsListAdapter adapter = new FriendsListAdapter(
-				getApplicationContext(), R.layout.found_tags_content_info,
+				getApplicationContext(), R.layout.friends_list_content_info,
 				friends);
 
 		listView.setAdapter(adapter);
 
+		setNormalOnClickListener(listView);
+	}
+	
+	public void setNormalOnClickListener(final ListView listView){
 		listView.setOnItemClickListener(new OnItemClickListener() {
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
@@ -51,13 +65,42 @@ public class FriendsListFragment extends Activity {
 		});
 	}
 	
+	
+	public void setDeleteOnClickListener(final ListView listView){
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				CheckBox checkBox = (CheckBox) view
+						.findViewById(R.id.friends_list_checkbox);
+				checkBox.toggle();
+				((FriendsListAdapter) listView.getAdapter())
+						.toggle(position);
+			}
+		});
+	}
+	
 	@Override
 	protected void onStart() {
 		super.onStart();
 		((FriendsListAdapter)listView.getAdapter()).notifyDataSetChanged();
 	}
 	
-	public void deleteFromFriendsList(View view) {
-		
+	public void deleteFromFriendsList(View view){
+		if(actualMode==NORMAL_MODE){
+			actualMode=DELETE_MODE;
+			((FriendsListAdapter)listView.getAdapter()).changeMode(actualMode);
+			setDeleteOnClickListener(listView);
+			confirmButton.setVisibility(LinearLayout.VISIBLE);
+		}
+		else if(actualMode==DELETE_MODE){
+			actualMode=NORMAL_MODE;
+			((FriendsListAdapter)listView.getAdapter()).changeMode(actualMode);
+			setNormalOnClickListener(listView);
+			confirmButton.setVisibility(LinearLayout.GONE);
+		}
+	}
+
+	public void confirmDeleteFromFriendsList(View view) {
+		((FriendsListAdapter)listView.getAdapter()).removeFromFriends();
 	}
 }
